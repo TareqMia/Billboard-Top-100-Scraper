f5ae604d9e3c421effc16ef221f902eba6786dfb
Tareq Mia <tareqmia81@gmail.com>
2020-02-24 00:09:26 -0500

    Billboard Top 5 Songs

---
+++ billboardScraper.py
@@ -0,0 +1,70 @@
+from bs4 import BeautifulSoup
+import requests
+
+source = requests.get("https://www.billboard.com/charts/hot-100").text
+
+soup = BeautifulSoup(source, "lxml")
+
+top_100_songs = soup.find_all("span", class_= "chart-element__information__song")
+top_100_artists = soup.find_all("span", class_= "chart-element__information__artist")
+
+songs = []
+artists = []
+
+for x in top_100_songs[0:5]:
+    songs.append(x.text)
+    
+for i in top_100_artists[0:5]:
+    artists.append(i.text)
+    
+zipped = list(zip(songs, artists))
+
+tweet = ""
+for index, x in enumerate(zipped):
+    tweet += f"{index + 1} {x[0]} by {x[1]} \n"
+    
+print(tweet)
+    
+    
+    
+
+    
+
+        
+    
+
+
+    
+    
+    
+
+
+
+
+
+
+
+
+    
+    
+    
+
+
+
+
+    
+
+
+
+
+
+
+    
+    
+
+
+
+
+
+
+
